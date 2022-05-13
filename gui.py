accaf991d5ea978e0ee73e2a2810689ef6c25d9d
szucsba <szucsbarnabas98@protonmail.com>
2022-05-13 15:40:52 +0200

    finetuned gui

---
+++ gui.py
@@ -0,0 +1,159 @@
+import signal
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+from tkinter import messagebox,filedialog
+from PIL import Image, ImageTk
+from functools import partial
+import cv2
+from igraph import Plot
+from max_clique_search import BpmnGraphParser, modular_product_graph, get_max_clique_bk2, color_similarity
+from tkinter import messagebox
+
+
+class Application(tk.Frame):
+    def __init__(self,master):
+        super().__init__(master)
+        self.pack()
+
+        self.master.geometry("1280x960")
+        self.master.title("BPMN similarity search utility")
+
+        self.image_bgr = dict()
+        self.height = dict()
+        self.width = dict()
+        self.new_size = dict()
+        self.image_bgr_resize = dict()
+        self.image_rgb = dict()
+        self.image_PIL = dict()
+        self.image_tk = dict()
+        self.filename = dict()
+        self.BPMNObject = dict()
+        self.GraphObject = dict()
+        self.label = dict()
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        #Canvases
+        self.canvas1 = tk.Canvas(self)
+        self.canvas1.configure(width=640, height=480, bg='orange')
+        self.canvas1.grid(column=1, row=0)
+
+        self.canvas2 = tk.Canvas(self)
+        self.canvas2.configure(width=640, height=480, bg='orange')
+        self.canvas2.grid(column=2, row=0)
+
+        self.label[self.canvas1] = Label(self)
+        self.label[self.canvas1].grid(column=1, row=1)
+        self.label[self.canvas2] = Label(self)
+        self.label[self.canvas2].grid(column=2, row=1)
+        #Frame
+        self.frame_button = ttk.LabelFrame(self)
+        self.frame_button.configure()
+        self.frame_button.grid(column=1,row=1)
+        self.frame_button.grid(padx=20, pady=20, columnspan=2)
+
+        #load 1 Button
+        self.button_open_bpmn_one = ttk.Button(self.frame_button)
+        self.button_open_bpmn_one.configure(text = 'Load input 1.')
+        self.button_open_bpmn_one.grid(column=0, row=1)
+        self.button_open_bpmn_one.configure(command=partial(self.loadBPMN,self.canvas1))
+
+        # Clear Button
+        self.start = ttk.Button( self.frame_button )
+        self.start.configure( text='Start search' )
+        self.start.grid(column=1, row=1 )
+        self.start.configure(command=self.startSearch)
+
+        # load2 Button
+        self.button_open_bpmn_two = ttk.Button( self.frame_button )
+        self.button_open_bpmn_two.config( text='Load input 2.' )
+        self.button_open_bpmn_two.grid(column=2, row=1 )
+        self.button_open_bpmn_two.configure(command=partial(self.loadBPMN,self.canvas2))
+
+        # Clear Button
+        self.clear = ttk.Button( self.frame_button )
+        self.clear.configure( text='Clear inputs' )
+        self.clear.grid(column=0, row=2, columnspan=4)
+        self.clear.configure(command=self.clearInputs)
+
+    # Event Call Back
+    def loadBPMN(self, canvas):
+        self.filename[canvas] = filedialog.askopenfilename()
+        self.label[canvas]['text'] = str(self.filename[canvas]).split('/')[-1]
+        self.label[canvas]['bg'] = "yellow"
+        self.BPMNObject[canvas] = BpmnGraphParser(self.filename[canvas])
+        self.GraphObject[canvas] = self.BPMNObject[canvas].get_graph()
+        plot = Plot("tmp.png", background="white")
+        plot.add(self.GraphObject[canvas], margin = 100)
+        plot.save()
+        self.loadImage(canvas)
+
+    # Event Call Back
+    def loadImage(self, canvas):
+        self.image_bgr[canvas] = cv2.imread("tmp.png")
+        self.height[canvas], self.width[canvas] = self.image_bgr[canvas].shape[:2]
+        
+        if self.width[canvas] > self.height[canvas]:
+            self.new_size[canvas] = (1280,960)
+        else:
+            self.new_size[canvas] = (640,480)
+
+        self.image_bgr_resize[canvas] = cv2.resize(self.image_bgr[canvas], self.new_size[canvas], interpolation=cv2.INTER_AREA)
+        self.image_rgb[canvas] = cv2.cvtColor( self.image_bgr_resize[canvas], cv2.COLOR_BGR2RGB )
+
+        self.image_PIL[canvas] = Image.fromarray(self.image_rgb[canvas]) 
+        self.image_tk[canvas] = ImageTk.PhotoImage(self.image_PIL[canvas])
+        canvas.create_image(320, 240, image=self.image_tk[canvas])
+
+    def clearInputs(self):
+        self.canvas1.delete("all")
+        self.canvas2.delete("all")
+        self.image_bgr = dict()
+        self.height = dict()
+        self.width = dict()
+        self.new_size = dict()
+        self.image_bgr_resize = dict()
+        self.image_rgb = dict()
+        self.image_PIL = dict()
+        self.image_tk = dict()
+        self.filename = dict()
+        self.BPMNObject = dict()
+        self.GraphObject = dict()
+        self.label[self.canvas1]['text'] = ""
+        self.label[self.canvas2]['text'] = ""
+
+    def startSearch(self):
+        if len(self.GraphObject) < 2:
+            messagebox.showwarning("Warning", "Please load in the BPMN XMLs!")
+            return
+        self.start['state'] = DISABLED
+        G, H = self.GraphObject.values()
+        MODULAR_PRODUCT_GRAPH = modular_product_graph(G, H)
+        signal.alarm(10)
+        max_clique = get_max_clique_bk2(MODULAR_PRODUCT_GRAPH)
+        signal.alarm(0)
+        color_similarity(MODULAR_PRODUCT_GRAPH, max_clique, G, H)
+
+        for canvas in self.GraphObject:
+            plot = Plot("tmp.png", background="white")
+            plot.add(self.GraphObject[canvas], margin = 100)
+            plot.save()
+            self.loadImage(canvas)
+        self.start['state'] = NORMAL
+
+    def quit_app(self):
+        self.Msgbox = tk.messagebox.askquestion("Exit Applictaion", "Are you sure?", icon="warning")
+        if self.Msgbox == "yes":
+            self.master.destroy()
+        else:
+            tk.messagebox.showinfo("Return", "you will now return to application screen")
+
+def main():
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
--- max_clique_search.py
+++ max_clique_search.py
@@ -4,7 +4,6 @@
 from __future__ import unicode_literals
 from utilities import BpmnGraphParser
 from timeit import default_timer as timer
-
 import igraph as ig
 import networkx as nx
 from tqdm import tqdm
@@ -12,26 +11,26 @@ from itertools import product
 from igraph import Plot
 import igraph as ig
 import os
+import signal
 from statistics import mean  
 from itertools import combinations
+import argparse
+
+
+parser = argparse.ArgumentParser(description='BPMN similarity exhaustive search')
+parser.add_argument('timeout', type=int, help='Give the timeout in seconds(!), defaults to 10 minutes.', default=600)
+
+class TimeOutException(Exception):
+    pass
+
+
+def signal_handler(signum, frame):
+    raise TimeOutException("Timed out!")
+
+signal.signal(signal.SIGALRM, signal_handler)
 
 
 helper_dict = dict()
-
-def color(G, P: set):
-    colorClasses = []
-
-    while P:
-        current = set()
-        for v in P:
-            if helper_dict.get(G.vs[v]) is None:
-                helper_dict[G.vs[v]] = set(G.neighbors(G.vs[v]))
-            if not current.intersection(helper_dict[G.vs[v]]):
-                current.add(v)
-        P = P.difference(current)
-        colorClasses += [current]
-
-    return colorClasses
 
 def bron_kerbosch1(M, clique, candidates, excluded, C):
     if not candidates and not excluded:
@@ -48,9 +47,9 @@ def bron_kerbosch1(M, clique, candidates, excluded, C):
         excluded.add(v)
     return C
 
-def bron_kerbosch2(M, clique: set, candidates, excluded, C):
+def bron_kerbosch2(M, clique: set, candidates, excluded):
     if not candidates and not excluded:
-        C += [clique]
+        yield clique
         return
  
     pivot = pick_random(M, candidates, excluded)
@@ -65,38 +64,14 @@ def bron_kerbosch2(M, clique: set, candidates, excluded, C):
         v_neighbors = helper_dict[M.vs[v]]
         new_candidates = candidates.intersection(v_neighbors)
         new_excluded = excluded.intersection(v_neighbors)
-        bron_kerbosch2(M, clique + [v], new_candidates, new_excluded, C)
+        yield from bron_kerbosch2(M, clique + [v], new_candidates, new_excluded)
         candidates.remove(v)
         excluded.add(v)
-    return C
-
-def branch_and_bound(M, clique: list, candidates: set, C: list):
-    colorClasses = color(M, candidates)
-    while (cclasses:=len(colorClasses)) != 0:
-        colorClass = colorClasses[0]
-        for v in colorClass:
-            cliqlen = len(clique)
-            clen = len(C)
-            if cliqlen + cclasses <= clen:
-                return
-
-            if helper_dict.get(M.vs[v]) is None:
-                helper_dict[M.vs[v]] = set(M.neighbors(M.vs[v]))
-            P1 = candidates.intersection(helper_dict[M.vs[v]])
-            clique.append(v)
-            if cliqlen > clen:
-                C = clique.copy()
-            if len(P1) != 0:
-                branch_and_bound(M, clique, P1, C)
-            clique.remove(v)
-        colorClasses.remove(colorClass)
 
 def pick_random(M, candidates: set, excluded: set):
     union = candidates.union(excluded)
     elem = union.pop()
     return elem
-    # elem = max(union, key=lambda x: len(intersect(candidates, M.neighbors(M.vs[x]))))
-    # return elem
   
 def node_product(G, H):
     def _dict_product(d1, d2):
@@ -108,6 +83,8 @@ def modular_product_graph(G: ig.Graph, H: ig.Graph) -> ig.Graph:
     print(f"Started modular product creation.")
     _G = G.to_networkx().to_undirected()
     _H = H.to_networkx().to_undirected()
+    _GD = G.to_networkx()
+    _HD = H.to_networkx()
     
     GH = nx.Graph()
     GH.add_nodes_from(node_product(_G, _H))
@@ -131,77 +108,105 @@ def modular_product_graph(G: ig.Graph, H: ig.Graph) -> ig.Graph:
                 h_inner_node_two_type = gh_node_candidate_two['type'][1]
                 if (g_inner_node_one_type != h_inner_node_one_type) or (g_inner_node_two_type != h_inner_node_two_type):
                     continue
-                GH.add_edge(node_i, node_j)
+                # TODO make it to filter edges to have the same direction
+                g_one_to_g_two = not set(_GD.out_edges(g_inner_node_one)).intersection(set(_GD.in_edges(g_inner_node_two)))
+                g_two_to_g_one = not set(_GD.out_edges(g_inner_node_two)).intersection(set(_GD.in_edges(g_inner_node_one)))
+                h_one_to_h_two = not set(_HD.out_edges(h_inner_node_one)).intersection(set(_HD.in_edges(h_inner_node_two)))
+                h_two_to_h_one = not set(_HD.out_edges(h_inner_node_two)).intersection(set(_HD.in_edges(h_inner_node_one)))
+                if (g_one_to_g_two and h_one_to_h_two) or (g_two_to_g_one and h_two_to_h_one):
+                    GH.add_edge(node_i, node_j)
             elif not adjacent_in_g and not adjacent_in_h:
                 if(g_inner_node_one == g_inner_node_two) or (h_inner_node_one == h_inner_node_two):
                     continue
                 GH.add_edge(node_i, node_j)
+
     igraph_format = ig.Graph.from_networkx(GH)
     igraph_format.vs.select(_degree=0).delete()
     print("Finished modular product creation.")
     return igraph_format
 
-def get_max_clique_bk1(input_graph):
+def get_max_clique_bk1(input_graph, timeout_sec=600):
     P = set([vertex.index for vertex in input_graph.vs()])
     R = []
     X = set()
     C = []
     start = timer()
-    C = bron_kerbosch1(input_graph, R, P, X, C)
+
+    try:
+        for clique in bron_kerbosch1(input_graph, R, P, X):
+            C.append(clique)
+    except TimeOutException:
+        print("Timeout!")
+
     end = timer()
     print(f"BK1 Elapsed time(msec): {(end - start)*1000}")
     max_clique = max(C, key=lambda clique: len(clique))
     return max_clique
 
-def get_max_clique_bk2(input_graph):
+def get_max_clique_bk2(input_graph, timeout_sec=600):
     P = set([vertex.index for vertex in input_graph.vs()])
     R = []
     X = set()
     C = []
     start = timer()
-    C = bron_kerbosch2(input_graph, R, P, X, C)
+
+    try:
+        for clique in bron_kerbosch2(input_graph, R, P, X):
+            C.append(clique)
+    except TimeOutException:
+        print("Timeout!")
+
     end = timer()
     print(f"BK2 Elapsed time(msec): {(end - start)*1000}")
     max_clique = max(C, key=lambda clique: len(clique))
     return max_clique
 
-def visualize_similarity(graph_candidate_one, graph_candidate_two, modular_product, max_clique_in_product, save_as_png=False):
+def color_similarity(modular_product, max_clique_in_product, graph_candidate_one, graph_candidate_two):
     vertex_set1 = set()
     vertex_set2 = set()
 
     for vertex_id in max_clique_in_product:
         first_graph_id = modular_product.vs[vertex_id]['id'][0]
         second_graph_id = modular_product.vs[vertex_id]['id'][1]
-        graph_candidate_one.vs[first_graph_id]['color'] = 'blue' 
-        graph_candidate_two.vs[second_graph_id]['color'] = 'blue'
         vertex_set1.add(first_graph_id)
         vertex_set2.add(second_graph_id)
-        
-    graph_candidate_one.es["color"] = ["red" if (edge.source in vertex_set1 and \
-                           edge.target in vertex_set1) else "black" \
-                 for edge in graph_candidate_one.es]
-    graph_candidate_two.es["color"] = ["red" if (edge.source in vertex_set1 and \
-                           edge.target in vertex_set1) else "black" \
-                 for edge in graph_candidate_one.es]
 
-    colsep, rowsep = 70, 70
-    width, height = 200, 200
+
+    mces_one = graph_candidate_one.es.select(_between = (vertex_set1, vertex_set1))
+    mces_two = graph_candidate_two.es.select(_between = (vertex_set2, vertex_set2))
+    mces_one['color'] = "red"
+    mces_two['color'] = "red"
+    mces_one['width'] = 5
+    mces_two['width'] = 5
+
+    for edge in mces_one:
+        graph_candidate_one.vs[edge.source]['color'] = 'blue'
+        graph_candidate_one.vs[edge.target]['color'] = 'blue'
+
+    for edge in mces_two:
+        graph_candidate_two.vs[edge.source]['color'] = 'blue'
+        graph_candidate_two.vs[edge.target]['color'] = 'blue'
+
+def visualize_similarity(graph_candidate_one, graph_candidate_two, modular_product, max_clique_in_product, output_filename, debug=False):
+    color_similarity(modular_product, max_clique_in_product, graph_candidate_one, graph_candidate_two)
+    colsep, rowsep = 100, 100
+    width, height = 400, 400
 
     # Construct the plot
-    plot = Plot("similarity.png", bbox=(4*width, 4*height), background="white")
+    plot = Plot(f"output/{output_filename}", bbox=(4*width, 4*height), background="white")
 
     # Create the graph and add it to the plot
     plot.add(graph_candidate_one, bbox=(colsep/2, rowsep/2, -colsep/2 + width*(2), -rowsep/2 + height*(2)))
     plot.add(graph_candidate_two, bbox=(colsep/2, rowsep/2 + height*(3), -colsep/2 + width*(3), -rowsep/2 + height*(2)))
-    plot.redraw()
 
-    plot.show()
-    if save_as_png:
-        plot.save()
+    plot.save()
+    if debug:
+        plot.redraw()
+        plot.show()
 
 
 if __name__ == "__main__":
-    #TODO add argparser
+    args = parser.parse_args()
     test_pairs = list(combinations(os.listdir("data/"), 2))
 
     for test_pair in test_pairs:
@@ -213,9 +218,12 @@ if __name__ == "__main__":
         MODULAR_PRODUCT_GRAPH = modular_product_graph(G, H)
         print(f"Moduláris szorzatgráf csúcsszáma: {len(MODULAR_PRODUCT_GRAPH.vs)}, átlagos szomszédszám: {mean([len(MODULAR_PRODUCT_GRAPH.neighbors(vertex)) for vertex in MODULAR_PRODUCT_GRAPH.vs])}")
         print("-----------------------------------------------------------------------")
-
-    # layout = MODULAR_PRODUCT_GRAPH.layout('grid')
-    # ig.plot(MODULAR_PRODUCT_GRAPH, layout=layout)
+        # signal.alarm(args.timeout)
+        max_clique = get_max_clique_bk2(MODULAR_PRODUCT_GRAPH)
+        print(f"Talált maximum klikk mérete: {len(max_clique)}")
+        print("-----------------------------------------------------------------------")
+        visualize_similarity(G, H, MODULAR_PRODUCT_GRAPH, max_clique, output_filename=f"{input_1_filename}-{input_2_filename}_similarity.png")
+        # layout = MODULAR_PRODUCT_GRAPH.layout('grid')
+        # ig.plot(MODULAR_PRODUCT_GRAPH, layout=layout)
 
     # max_clique = get_max_clique_bk2(MODULAR_PRODUCT_GRAPH)
-    # visualize_similarity(graph_candidate_one, graph_candidate_two, MODULAR_PRODUCT_GRAPH, max_clique)
